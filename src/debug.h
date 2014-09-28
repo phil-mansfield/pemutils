5f791c7351f7cf4c0efcc07784ac24a928bf3fbc
Phil Mansfield <pmansfie@andrew.cmu.edu>
2014-09-28 16:46:10 -0500

    Finished first version of debug.h.

--- src/debug.h
+++ src/debug.h
@@ -4,16 +4,19 @@
 #include <stdio.h>
 
 #ifdef DEBUG
-#define debug_assert()
+#define debug_assert(expr) assert(expr)
 #define debug_printf(fmt, ...) printf(fmt, ##__VA_ARGS__)
 #else /*!DEBUG*/
-#define debug_assert()
+#define debug_assert(expr)
 #define debug_printf(fmt, ...)
 #endif /* DEBUG */
 
-#define print_location() printf("%s:%s:%d", __FILE__, __PRETTY_FUNCTION__, __LINE__)
-#define println_location() printf("%s:%s:%d\n", __FILE__, __PRETTY_FUNCTION__, __LINE__)
+#define fprint_location(loc) fprintf(loc, "%s: %s: line %d: ", __FILE__, __PRETTY_FUNCTION__, __LINE__)
+#define println_location() printf("%s: %s: line %d\n", __FILE__, __PRETTY_FUNCTION__, __LINE__)
 
-#define check_mem(ptr)
+#define check(expr, fmt, ...) (if (expr) {fprint_location(stderr); fprintf(stderr, fmt, ##__VA_ARGS__);  exit(1);})
+
+#define check_mem(ptr) check(ptr, "Allocator failed :3")
+#define impossible(fmt, ...) check(0, fmt, ##__VA_ARGS__)
 
 #endif /* PEMUTILS_DEBUG_H_ */
